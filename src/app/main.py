d42ec83abe221bc8a4266e8460c9da2235811c96
Alberto Montemiglio <71413117+alberto-montemiglio@users.noreply.github.com>
2025-10-08 09:55:15 +0100

    Add graph_db router

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from app.routers import graph_db
 
 app = FastAPI()
+app.include_router(graph_db.router)
 
 
 # Redirect root to docs
