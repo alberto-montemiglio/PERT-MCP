b798fda7b8be86870a6903d0b727de661a0546d0
Alberto Montemiglio <71413117+alberto-montemiglio@users.noreply.github.com>
2025-10-10 19:38:21 +0100

    Add Neo4j DB driver as lifespan event

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,24 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.driver = AsyncGraphDatabase.driver(
+        uri=get_env_variable("NEO4J_DB_URI"),
+        auth=Auth("basic", get_env_variable("NEO4J_DB_USER"), get_env_variable("NEO4J_DB_PASSWORD"))
+    )
+    yield
+    await app.state.driver.close()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(events.router)
+app.include_router(activities.router)
 
 # Redirect root to docs
 @app.get("/")
