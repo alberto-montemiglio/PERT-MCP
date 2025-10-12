d9fd32f2f571c9605674e41b6dfffcd249d16e25
Alberto Montemiglio <71413117+alberto-montemiglio@users.noreply.github.com>
2025-10-12 18:55:15 +0100

    Add Service for graph plotting

--- src/app/services.py
+++ src/app/services.py
@@ -1,7 +1,16 @@
 import asyncio
+import tempfile
+from typing import Annotated
 
-from fastapi import FastAPI
+import matplotlib.pyplot as plt
+import networkx as nx
+from fastapi import Depends, FastAPI
+from fastapi.responses import FileResponse
 from loguru import logger
+from neo4j import AsyncSession
+
+from app.dependencies import get_async_db_session
+from app.schemas import Activity, Event
 
 
 async def wait_for_db_connection(app: FastAPI):
@@ -37,3 +46,71 @@ async def create_event_uniqueness_constraints(app: FastAPI):
     except Exception as e:
         logger.error(f"Error creating uniqueness constraints: {e}")
         raise RuntimeError("Failed to create uniqueness constraints") from e
+
+
+async def get_graph(
+    session=Annotated[AsyncSession, Depends(get_async_db_session)],
+) -> FileResponse:
+    G = nx.Graph()
+    query = """
+    MATCH (n)-[r]->(m)
+    RETURN n, r, m
+    """
+    result = await session.run(query)
+    data = await result.data()
+
+    for nrm in data:
+        n, r, m = nrm["n"], nrm["r"], nrm["m"]
+        n_event = Event(id=str(n["id"]), name=n["name"])
+        m_event = Event(id=str(m["id"]), name=m["name"])
+        r_activity = Activity(
+            id=r[0]["id"],
+            name=r[0].get("name", ""),
+            duration=r[0].get("duration", 0),
+            cost=r[0].get("cost", 0),
+            from_event_id=r[0]["from_event_id"],
+            to_event_id=r[0]["to_event_id"],
+        )
+
+        G.add_node(n_event.id, **n_event.dict())
+        G.add_node(m_event.id, **m_event.dict())
+        G.add_edge(n_event.id, m_event.id, **r_activity.dict())
+
+    plt.figure(figsize=(12, 8))
+    pos = nx.spring_layout(G)
+    node_labels = {
+        node: "ID: " + data.get("id", "") + "\nName: " + data.get("name", "")
+        for node, data in G.nodes(data=True)
+    }
+    nx.draw_networkx(
+        G,
+        pos,
+        with_labels=True,
+        labels=node_labels,
+        node_color="lightblue",
+        node_size=2000,
+        font_size=10,
+        font_weight="bold",
+        arrows=True,
+    )
+
+    edge_labels = {
+        (u, v): "Name: "
+        + d.get("name", "")
+        + "\nFrom Event "
+        + d.get("from_event_id", "")
+        + "\nTo Event "
+        + d.get("to_event_id", "")
+        + "\nDuration: "
+        + str(d.get("duration", 0))
+        + "\nCost: "
+        + str(d.get("cost", 0))
+        for u, v, d in G.edges(data=True)
+    }
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmp_file:
+        plt.savefig(tmp_file.name, format="png")
+        plt.close()
+
+        return FileResponse(tmp_file.name, media_type="image/png", filename="graph.png")
