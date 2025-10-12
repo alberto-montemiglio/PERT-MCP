8dbfa1a90ed562f93f461071ff6aa8bc1291be51
Alberto Montemiglio <71413117+alberto-montemiglio@users.noreply.github.com>
2025-10-12 18:58:32 +0100

    Add connection check and event ID uniqueness enforcement to lifespan function

--- src/app/main.py
+++ src/app/main.py
@@ -2,16 +2,30 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from neo4j import AsyncGraphDatabase, Auth
+
+from app.routers import activities, events
+from app.services import create_event_uniqueness_constraints, wait_for_db_connection
+from utils import get_env_variable
 
 app = FastAPI()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    """
+    Initialize and close the database driver and create uniqueness constraints for Events and Activities IDs.
+    """
     app.state.driver = AsyncGraphDatabase.driver(
         uri=get_env_variable("NEO4J_DB_URI"),
-        auth=Auth("basic", get_env_variable("NEO4J_DB_USER"), get_env_variable("NEO4J_DB_PASSWORD"))
+        auth=Auth(
+            "basic",
+            get_env_variable("NEO4J_DB_USER"),
+            get_env_variable("NEO4J_DB_PASSWORD"),
+        ),
     )
+    await wait_for_db_connection(app)
+    await create_event_uniqueness_constraints(app)
     yield
     await app.state.driver.close()
 
@@ -19,6 +33,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(events.router)
 app.include_router(activities.router)
+
 
 # Redirect root to docs
 @app.get("/")
